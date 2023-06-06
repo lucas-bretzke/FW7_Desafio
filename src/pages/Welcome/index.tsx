aef2bc85b4c3e175e4dbcc15199c4bd2727d12fe
Lucas Bretzke <lucasbretzke@gmail.com>
2023-06-05 22:54:59 -0300

    feat: screen Welcome

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
 import styles from './styles';
+import { Feather } from '@expo/vector-icons';
 
-export default function Home() {
+import * as Animatable from 'react-native-animatable'
+
+
+type WelcomeTypes = {
+    navigation: any
+}
+
+export default function Welcome({ navigation }: WelcomeTypes) {
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Welcome Page</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Animatable.View animation="flipInY" style={styles.containerLogo} >
+                <Feather name="link" size={26} color="blue" />
+                <Text style={styles.title}>Encurtador de URL</Text>
+            </Animatable.View>
+
+            <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.desc1}>Transforme URLs longas em curtas com facilidade!</Text>
+                <Text style={styles.desc2}>Organize seus links com a Bretz.</Text>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate('Home')}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </Animatable.View>
+        </SafeAreaView>
     );
-} 
+}
--- src/pages/Welcome/styles.tsx
+++ src/pages/Welcome/styles.tsx
@@ -1,13 +1,59 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
+
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        backgroundColor: '#192436'
+    },
+    containerLogo: {
+        flex: 2,
+        display: 'flex',
+        flexDirection: 'row',
         justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#192436',
     },
     title: {
-        fontSize: 28
+        fontSize: 26,
+        color: 'white',
+        marginLeft: 10,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#023696',
+        paddingVertical: 12,
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '4%',
+        paddingEnd: '4%',
+    },
+    desc1: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 22,
+        marginBottom: 12,
+    },
+    desc2: {
+        fontSize:17,
+    },
+    button: {
+        position: 'absolute',
+        backgroundColor: '#023696',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: '12%',
+        alignItems: 'center',
+        justifycontent: 'center'
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#fff',
+        fontWeight: 'bold'
     }
 })
