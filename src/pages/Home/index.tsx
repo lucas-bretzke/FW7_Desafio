8c10b59063188563197923493f298d03e4b70a08
Lucas Bretzke <lucasbretzke@gmail.com>
2023-06-06 00:34:42 -0300

    feat: Shorten url screen 30% done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
 import styles from './styles';
+import { Feather } from '@expo/vector-icons';
+
+
+import Button from '../../components/Button'
 
 export default function Home() {
+    const [url, setUrl] = useState('');
+    const [newUrl, setNewUrl] = useState('');
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Home Page</Text>
+            <View style={styles.urlFormContainer}>
+
+                <View style={styles.containerLogo} >
+                    <Feather name="link" size={26} color="blue" />
+                    <Text style={styles.title}>Encurtador de URL</Text>
+                </View>
+
+                <View style={styles.containerInput}>
+                    <Text style={styles.label}>Destino</Text>
+                    <TextInput
+                        value={url}
+                        onChangeText={text => setUrl(text)}
+                        placeholder='Coloque sua URL aqui'
+                        style={styles.input}
+                    />
+                </View>
+                <View style={styles.containerInput}>
+                    <Text style={styles.label}>Títilo (opcional)</Text>
+                    <TextInput
+                        value={url}
+                        onChangeText={text => setNewUrl(text)}
+                        style={styles.input}
+                    />
+                </View>
+
+                <View style={styles.containerButtons}>
+                    <Button onClick={() => alert('Clicou')} text="Encurtar" color="#fff" bgColor="#023696" width={145} />
+                    <Button onClick={() => alert('Clicou')} text="Copiar" color="orange" bgColor="#023696" width={145} />
+                </View>
+
+            </View>
+
+            <View style={styles.containsListOfLinks}>
+                LIST
+            </View>
         </View>
     );
 }
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -3,11 +3,72 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#192436',
+        display: 'flex',
+        flexdirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
     },
+
+    urlFormContainer: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    containerLogo: {
+        flex: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#192436',
+    },
     title: {
-        fontSize: 28
+        fontSize: 26,
+        color: 'white',
+        marginLeft: 10,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#023696',
+        paddingVertical: 12,
+    },
+
+    containerInput: {
+        width: '90%',
+        marginHorizontal: 'auto',
+    },
+
+    label: {
+        fontWeight: '600',
+        color: 'white'
+    },
+
+    input: {
+        width: '100%',
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 12,
+        marginTop: 5,
+        marginBottom: 12,
+        fontSize: 16,
+        backgroundColor: 'white'
+    },
+
+    containerButtons: {
+        width: '100%',
+        marginTop: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    containsListOfLinks: {
+        flex: 1,
+        width: '100%',
+        borderWidth: 1,
     }
 })
