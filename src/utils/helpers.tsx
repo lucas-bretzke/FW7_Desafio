cf39f47d4e2dc3f20b7a780b99997712e9f36691
Lucas Bretzke <lucasbretzke@gmail.com>
2023-09-23 11:28:58 -0300

    feat(create-login-screen): added functions validateTheEmail and urlValidator

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,6 +1,12 @@
-const urlValidator = (url: string) => {
-    const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-    return regex.test(url);
+const validateTheEmail = (email: any) => {
+  var re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
 }
 
-export { urlValidator }
+const urlValidator = (url: string) => {
+  const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i
+  return regex.test(url)
+}
+
+export { validateTheEmail, urlValidator }
