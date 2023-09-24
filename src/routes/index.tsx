c1b59147ad475dc9dd98a9ddd9830a64469059c2
Lucas Bretzke <lucasbretzke@gmail.com>
2023-09-24 17:23:48 -0300

    feat(create-registration-screen)  added route Register

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import Welcome from '../pages/Welcome'
+/**
+ * Screens
+ */
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import Welcome from '../pages/Welcome'
+import Register from '../pages/Register'
 
 const Stack = createNativeStackNavigator()
 
@@ -16,6 +20,12 @@ export default function Routes() {
           component={Login}
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name='Register'
+          component={Register}
+          options={{ headerShown: false }}
+        />
+
         <Stack.Screen
           name='Welcome'
           component={Welcome}
