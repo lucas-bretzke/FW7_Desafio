b117283865a3fb1e20f1b314da880c52e85b85d6
Lucas Bretzke <lucasbretzke@gmail.com>
2023-09-24 15:10:09 -0300

    feat(create-login-screen) added route of login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Welcome from '../pages/Welcome'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
 
 const Stack = createNativeStackNavigator()
 
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-                <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Welcome'
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
