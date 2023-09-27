f11e065becc9c142ab6798488114bba4874a1ea2
Lucas Bretzke <lucasbretzke@gmail.com>
2023-09-27 14:31:12 -0300

    feat(create-registration-screen)  added Spinner/loading and methods: createAccount, validateForm

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,14 +1,28 @@
+import { Alert, View } from 'react-native'
 import * as Animatable from 'react-native-animatable'
 import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+
+/**
+ * Services.
+ */
+import api from '../../services/api'
+
+/**
+ * Utils.
+ */
+import { validateTheEmail } from '../../utils/helpers'
+
+/**
+ * Components.
+ */
+import Buttom from '../../components/Form/Buttom'
+import InputText from '../../components/Form/InputText'
+import InputPassword from '../../components/Form/InputPassword'
 
 /**
  * Styles.
  */
-import styles, { Container, Title } from './styles'
-import InputText from '../../components/Form/InputText'
-import Buttom from '../../components/Form/Buttom'
-import InputPassword from '../../components/Form/InputPassword'
+import styles, { Container, Spinner, Title } from './styles'
 
 /**
  * Component.
@@ -16,49 +30,97 @@ import InputPassword from '../../components/Form/InputPassword'
 export default function Register() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState(false)
   const [password, setPassword] = useState('')
+  const [msgError, setMsgError] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
-  function createAccount() {}
+  const validateForm = () => {
+    if (!name) {
+      setMsgError('Preencha o nome')
+      return false
+    }
+
+    if (!validateTheEmail(email)) {
+      setMsgError('email inválido')
+      return false
+    }
+
+    if (password.length > 1 && password !== confirmPassword) {
+      setMsgError('As senha não são iguais')
+      return false
+    }
+
+    if (password.length > 5 && password === confirmPassword) setMsgError('')
+    return true
+  }
+
+  async function createAccount() {
+    try {
+      setLoading(true)
+      if (validateForm()) {
+        await api.createAccount(name, email, password)
+        Alert.alert(
+          'Conta criada com sucesso!',
+          'Sua conta foi criada com sucesso.',
+          [
+            {
+              text: 'Logar',
+              onPress: () => console.log('teste')
+            }
+          ]
+        )
+      }
+    } catch (error) {
+      setMsgError('Erro interno, volte mais tarde')
+      console.log('Erro ao criar a conta', error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
-    <Container>
-      <Animatable.View delay={1000} animation='fadeInUp'>
-        <Title style={styles.containerHeader}>Crie sua conta</Title>
+    <>
+      <Container>
+        <Animatable.View delay={1000} animation='fadeInUp'>
+          <Title style={styles.containerHeader}>Crie sua conta</Title>
 
-        <View style={styles.containerForm}>
-          <InputText
-            value={name}
-            label='Nome'
-            icon='user'
-            placeholder='Nome'
-            onChangeText={text => setName(text)}
-          />
+          <View style={styles.containerForm}>
+            <InputText
+              value={name}
+              label='Nome'
+              icon='user'
+              placeholder='Nome'
+              onChangeText={text => setName(text)}
+            />
 
-          <InputText
-            value={email}
-            label='E-mail'
-            icon='mail'
-            placeholder='example@gmail.comn'
-            onChangeText={text => setEmail(text)}
-          />
-          <InputPassword
-            value={password}
-            onChangeText={text => setPassword(text)}
-          />
-          <InputPassword
-            value={confirmPassword}
-            onChangeText={text => setConfirmPassword(text)}
-          />
+            <InputText
+              value={email}
+              label='E-mail'
+              icon='mail'
+              placeholder='example@gmail.comn'
+              onChangeText={text => setEmail(text)}
+            />
+            <InputPassword
+              value={password}
+              onChangeText={text => setPassword(text)}
+            />
+            <InputPassword
+              value={confirmPassword}
+              onChangeText={text => setConfirmPassword(text)}
+              msgError={msgError}
+            />
 
-          <Buttom
-            title={'Confirmar'}
-            onPress={() => console.log('ddd')}
-            disabled={false}
-            style={{ marginTop: 20 }}
-          />
-        </View>
-      </Animatable.View>
-    </Container>
+            <Buttom
+              title={'Confirmar'}
+              onPress={() => createAccount()}
+              disabled={false}
+              style={{ marginTop: 20 }}
+            />
+          </View>
+        </Animatable.View>
+      </Container>
+      {loading && <Spinner size='large' color='black' />}
+    </>
   )
 }
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -13,6 +13,17 @@ export const Title = styled.Text`
   color: #fff;
 `
 
+export const Spinner = styled.ActivityIndicator`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: white;
+  opacity: 0.2;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
 export default StyleSheet.create({
   containerHeader: {
     marginVertical: '14%',
