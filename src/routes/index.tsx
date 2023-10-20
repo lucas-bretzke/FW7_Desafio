a9f38917bee60cae81a73a7d0d7e60fe108e14b3
Lucas Bretzke <lucasbretzke@gmail.com>
2023-10-20 07:01:34 -0300

    feat: Integrated dropdown

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,79 +1,71 @@
-import { Ionicons } from '@expo/vector-icons'
 import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-/**
- * Screens
- */
+// Screens.
 import Login from '../pages/Login'
 import Welcome from '../pages/Welcome'
 import CreateAccount from '../pages/CreateAccount'
 import SavedLinksScreen from '../pages/SavedLinksScreen'
 import CreateNewLinkScreen from '../pages/CreateNewLinkScreen'
-import { View, Text, TouchableOpacity } from 'react-native'
 import AuthWrapper from './AuthWrapper'
 
+const Drawer = createDrawerNavigator()
 const Stack = createNativeStackNavigator()
 
-// function CustomHeader({ navigation }: any) {
-//   return (
-//     <View
-//       style={{
-//         height: 60,
-//         flexDirection: 'row',
-//         alignItems: 'center',
-//         paddingHorizontal: 10
-//       }}
-//     >
-//       <TouchableOpacity onPress={() => navigation.openDrawer()}>
-//         <Ionicons name='menu' size={30} color='black' />
-//       </TouchableOpacity>
-//       <Text style={{ fontSize: 20, marginLeft: 30 }}>Links</Text>
-//     </View>
-//   )
-// }
+function DrawerNavigation() {
+  return (
+    <Drawer.Navigator initialRouteName='SavedLinksScreen'>
+      <Drawer.Screen
+        name='SavedLinksScreen'
+        component={SavedLinksScreen}
+        options={{ headerShown: false }}
+      />
+    </Drawer.Navigator>
+  )
+}
+
+function StackNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='AuthWrapper'
+        component={AuthWrapper}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='Welcome'
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='CreateNewLinkScreen'
+        component={CreateNewLinkScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='Login'
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='CreateAccount'
+        component={CreateAccount}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='SavedLinksScreen'
+        component={DrawerNavigation}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='AuthWrapper'
-          component={AuthWrapper}
-          options={{ headerShown: false }}
-        />
-
-        <Stack.Screen
-          name='Welcome'
-          component={Welcome}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='CreateNewLinkScreen'
-          component={CreateNewLinkScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='SavedLinksScreen'
-          component={SavedLinksScreen}
-          options={{ headerShown: false }}
-
-          // options={{
-          //   header: props => <CustomHeader {...props} />
-          // }}
-        />
-
-        <Stack.Screen
-          name='Login'
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='CreateAccount'
-          component={CreateAccount}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <StackNavigation />
     </NavigationContainer>
   )
 }
